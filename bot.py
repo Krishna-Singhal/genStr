ffc48e8a116687e48b226e8803de67693a9be406
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-11-25 10:26:43 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+
+from heroku3 import from_key
+from pyrogram import Client
+
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+APP_NAME = os.environ.get("APP_NAME", None)
+API_KEY = os.environ.get("API_KEY", None)
+HU_APP = from_key(API_KEY).apps()[APP_NAME]
+       
+bot = Client(":memory:",
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN)
