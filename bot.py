6b03ad785bd7c613293093a5375c6664b65be511
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-11-25 10:45:50 +0530

    fixed

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 APP_NAME = os.environ.get("APP_NAME", None)
 API_KEY = os.environ.get("API_KEY", None)
 HU_APP = from_key(API_KEY).apps()[APP_NAME]
-       
+
 bot = Client(":memory:",
              api_id=API_ID,
              api_hash=API_HASH,
