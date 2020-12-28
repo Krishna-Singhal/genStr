8b4b535b0649dedc7667bad0d96edd6e6c29c5d4
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-12-28 18:27:44 +0530

    fixed

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ class bot(Client):
     def __init__(self):
         super().__init__(
             ":memory:",
-            api_id=Config.APP_ID,
+            api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN
         )
